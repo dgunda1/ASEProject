d5521172122b8745e4dec6e30a6b853f6a9fae33
dgunda1 <dgunda.gsu@gmail.com>
2017-04-21 00:29:58 -0400

    Add files via upload

---
+++ src/test/java/com/asedemo/jenkins/DemoforMaven.java
@@ -0,0 +1,13 @@
+package com.asedemo.jenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoforMaven {
+
+	@Test
+	public void testHello() {
+		
+		System.out.println("Hello World..!");
+	}
+	
+}
---
+++ src/test/java/com/asedemo/jenkins/Testfacebook.java
@@ -0,0 +1,22 @@
+package com.asedemo.jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Testfacebook {
+
+@Test
+public void TestFireFox(){
+
+WebDriver driver=new FirefoxDriver();
+
+driver.manage().window().maximize();
+
+driver.get("http://www.facebook.com");
+
+driver.quit();
+
+}
+
+}	
